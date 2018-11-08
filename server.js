7e316a0ca64d2989a7d7bd9504fc37e108a9d174
Farihah Kabir <nadahkabir@gmail.com>
2018-11-08 03:03:53 +0000

    run html file

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 var http = require('http');
-  var server = http.createServer(function(req, res){
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end("Hello World\n");
+var fs = require('fs');
+
+var server = http.createServer(function(req, res){
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  fs.readFile('index.html', function(err, data){
+    if(err){
+      return console.log("File read error");
+    }
+    res.end(data);
   });
-  server.listen(process.env.PORT, process.env.IP, function(){
-    console.log('Server running');
-  });
+  //res.end("Hello World\n");
+});
+
+server.listen(process.env.PORT || 3000, process.env.IP, function(){ //c9 has predefined port (process.env.PORT) but if you use a local host, port 3000
+  console.log('Server running');
+});
