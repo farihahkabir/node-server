5ea56f262345e1402e54f1e3e4cf2066ba370d13
Farihah Kabir <nadahkabir@gmail.com>
2018-11-12 03:05:09 +0000

    Classwork 8a

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 var http = require('http');
-var fs = require('fs');
+var express = require('express');
+var app = express();
+var server = http.Server(app);
 
-var server = http.createServer(function(req, res){
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  fs.readFile('index.html', function(err, data){
-    if(err){
-      return console.log("File read error");
-    }
-    res.end(data);
-  });
-  //res.end("Hello World\n");
+app.get('/', function(request, response){
+  response.sendFile(__dirname+'/index.html');
 });
+
+// var fs = require('fs');
+
+// var server = http.createServer(function(req, res){
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/html');
+//   fs.readFile('index.html', function(err, data){
+//     if(err){
+//       return console.log("File read error");
+//     }
+//     res.end(data);
+//   });
+//   //res.end("Hello World\n");
+// });
 
 server.listen(process.env.PORT || 3000, process.env.IP, function(){ //c9 has predefined port (process.env.PORT) but if you use a local host, port 3000
   console.log('Server running');
