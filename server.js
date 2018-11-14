55ac45e5cc997771346856249e41ee17b8b3874f
Farihah Kabir <nadahkabir@gmail.com>
2018-11-14 03:30:23 +0000

    Classword 8b

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ var http = require('http');
 var express = require('express');
 var app = express();
 var server = http.Server(app);
+var bodyParser = require('body-parser');
+
+//configuring app with bodyParser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/', function(request, response){
   response.sendFile(__dirname+'/index.html');
@@ -10,6 +15,25 @@ app.get('/', function(request, response){
 app.get('/about-page', function(request, response){
   response.sendFile(__dirname+'/about.html');
 });
+
+app.get('/new-article', function(request, response){
+  response.sendFile(__dirname+'/form.html');
+});
+
+var article = [];
+
+app.post('/article/create', function(request, response){
+  console.log(request.body);//when you get a request you need to send back a response to client
+  if(!request.body.title){
+    return response.status(400).json({error: "Please add a title"});
+  }
+  article.push(request.body);
+  return response.status(200).json({message: "Article successfully created"});
+});
+
+app.get('/article/list', function(request, response) {
+    return response.status(200).json({articles: article});
+})
 
 // var fs = require('fs');
 
